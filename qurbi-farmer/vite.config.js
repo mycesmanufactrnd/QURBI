13a5cb8ff8c94c71da5da991cb124e1de6c658ac
naxiatul <iqqa2112@gmail.com>
2026-09-14 13:14:40 +0800

    localhost port

--- qurbi-farmer/vite.config.js
+++ qurbi-farmer/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   plugins: [
     base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
